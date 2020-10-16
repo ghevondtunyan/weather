979c1f3ff143d007c5cdb926ac3bf9e9a63f1ff0
ghevondtunyan <60566859+ghevondtunyan@users.noreply.github.com>
2020-10-16 15:04:55 +0400

    Updates

---
+++ precache-manifest.7614a59302305ea083e3a3a4b8f04cc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70e96f53e9411337a89282f93dd63fd",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "9eda634905ef9f83e7f1",
+    "url": "/weather/static/js/2.c3c0664e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/weather/static/js/2.c3c0664e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6032632353234e6457e",
+    "url": "/weather/static/js/main.4a26d7a2.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
